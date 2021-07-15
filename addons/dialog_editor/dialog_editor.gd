81f4a67b661372ca40a3813f72e87c1a67bcf9c9
Pedro Reis <pedroreis.ad@protonmail.com>
2021-07-15 14:27:44 +0100

    mequie, siga nisso

--- addons/dialog_editor/dialog_editor.gd
+++ addons/dialog_editor/dialog_editor.gd
@@ -11,16 +11,25 @@ func _enter_tree():
 	get_editor_interface().get_editor_viewport().add_child(main_panel_instance)
 	# Hide the main panel. Very much required.
 	make_visible(false)
+	print("fosgasse")
+	ProjectSettings.set("addons/teste", 0)
 
+	var property_info = {
+		"name": "addons/teste",
+		"type": TYPE_INT,
+		"hint": PROPERTY_HINT_ENUM,
+		"hint_string": "one,two,three"
+	}
+
+	ProjectSettings.add_property_info(property_info)
+	print(ProjectSettings)
 
 func _exit_tree():
 	if main_panel_instance:
 		main_panel_instance.queue_free()
 
-
 func has_main_screen():
 	return true
-
 
 func make_visible(visible):
 	if main_panel_instance:
@@ -29,7 +38,6 @@ func make_visible(visible):
 
 func get_plugin_name():
 	return "Dialog Editor"
-
 
 func get_plugin_icon():
 	# Must return some kind of Texture for the icon.
